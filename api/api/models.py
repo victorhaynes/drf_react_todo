ae8a4b352498a1e2c343aee6b9feeca25f6bb44a
Victor Haynes <haynesvic@gmail.com>
2023-08-22 14:32:09 -0500

    configured admin panel, corrected start_date field for Task model, ran migrations

--- api/api/models.py
+++ api/api/models.py
@@ -1,11 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
-    start_date = models.DateTimeField.auto_now_add=True
+    start_date = models.DateTimeField(default=timezone.now)
     end_date = models.DateTimeField(null=True, blank=True)
     completed = models.BooleanField
 
     def __str__(self):
         return f"{self.title}: {self.description}"
+    
+    def days_remaining(self):
+        if self.start_date and self.end_date:
+            days_left = self.end_date - self.start_date
+            return f"There are {days_left} days remaining for this task."
+        else:
+            return f"Please ensure that the start and end dates are populated."
