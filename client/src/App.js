773319556b0ee8fe8ba5afbe3378d80218a5d50c
Victor Haynes <haynesvic@gmail.com>
2023-08-31 13:05:19 -0500

    Added component to handle private route on react front end

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home'
 import LoginPage from './components/LoginPage'
+import Header from './components/Header'
+import NotFound404 from './components/NotFound404';
+import PrivateRoutes from './utils/PrivateRoutes';
 import {
   BrowserRouter as Router,
   Routes,
@@ -12,12 +15,16 @@ import {
 
 function App() {
   return (
-      <Router>
+    <Router>
+      <Header/>
         <Routes>
           <Route Component={Home} path="/" exact/>
           <Route Component={LoginPage} path="/login"/>
+          <Route Component={PrivateRoutes}>
+            <Route Component={NotFound404} path="/abc"/>
+          </Route>
         </Routes>
-      </Router>
+    </Router>
   );
 }
 
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link} from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div>
+        <Link to="/">Home</Link>
+        <span> | </span>
+        <Link to="/login">Login</Link>
+        <span> | </span>
+        <Link to="/abc">Private Route</Link>
+    </div>
+  )
+}
+
+export default Header
